8c5bfb8afadba503a2e537ec41202481d8f983d0
Dylan J <dylan@tritan.dev>
2023-01-02 22:27:54 -0500

    pushing im tired

--- Controllers/verifyemail.go
+++ Controllers/verifyemail.go
@@ -1,8 +1,46 @@
 package controllers
 
-import "github.com/gofiber/fiber"
+import (
+	"io/ioutil"
+	"log"
 
-func VerifyEmail(ctx *fiber.Ctx){
-	ctx.Send("uwu")
+	"github.com/gofiber/fiber"
+	"gopkg.in/yaml.v3"
+)
+
+func VerifyEmail(ctx *fiber.Ctx) {
+	email := ctx.Params("email")
+	user_auth := ctx.Query("token")
+	config_file, config_err := ioutil.ReadFile("config.yaml")
+	if config_err != nil {
+		log.Fatalf("Config Error: %v", config_err)
+	}
+
+	var config Config
+	parsing_err := yaml.Unmarshal(config_file, &config)
+	if parsing_err != nil {
+		log.Fatalf("Config Parsing Error: %v", parsing_err)
+	}
+
+	isAuth := false
+	for _, item := range config.auth_keys {
+		if item == user_auth {
+			isAuth = true
+			break
+		}
+	}
+
+	if isAuth == false {
+		ctx.JSON(fiber.Map{
+			"error":   true,
+			"code":    403,
+			"message": "You are not authenticated.",
+		})
+	} else {
+		// do stuff
+		ctx.JSON(fiber.Map{
+			"uwu?":  "uwu indeed",
+			"email": email,
+		})
+	}
 }
-
---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+## A very **very** basic email auth microservice made for a friend's personal purposes.
+
+API Routes
+
+[Root]
+GET / - What do you expect? It's an api landing page
+
+[API]
+GET /api/verify/[email]?token=[token]
+GET /api/check/[email]?token=[token]
